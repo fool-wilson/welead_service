467ffb1497ebfd06c5a3c3cd64bc399e9fd26e9c
psychoticvenom <x231231x@gmail.com>
2020-11-24 19:57:35 +0800

    router header add option to allow methods

--- src/router.js
+++ src/router.js
@@ -5,7 +5,7 @@ const methods = require('./methods/methods')
 router.use((req, res, next) => {
   res.append('Access-Control-Allow-Origin', '*');
   res.append('Access-Control-Allow-Credentials', 'true');
-  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTION');
   res.append('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
